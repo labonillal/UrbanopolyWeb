1d6d1c81f29118c9112f0218da14e573e6d52407
Luis Alberto Bonilla Leon <bonilla.luisalberto@gmail.com>
2014-07-30 15:48:28 +0200

    formatCurrency added

--- v1.4/public/javascripts/venuesView.js
+++ v1.4/public/javascripts/venuesView.js
@@ -2,7 +2,7 @@ var selectedVenue;
 
 $('table tr').click(function(event) {
     // Get id of the clicked venue
-    console.log('Row clicked!: ' + $(event.target).closest('tr').data('id'));
+    //console.log('Row clicked!: ' + $(event.target).closest('tr').data('id'));
     var venueId = $(event.target).closest('tr').data('id');
     //Default setup
     $("#buyBtn").hide();
@@ -18,14 +18,14 @@ $('table tr').click(function(event) {
 			type: 'GET',
 			url: '/retrieveVenueDetails?venueId=' + venueId,
 			success: function (venueDetails) {
-				console.log('VENUE DETAILS JSON: ', JSON.stringify(venueDetails));
+				//console.log('VENUE DETAILS JSON: ', JSON.stringify(venueDetails));
 				if(venueDetails.length != 0){
 					selectedVenue = JSON.stringify(venueDetails);
 					$("#venueName").text(venueDetails.name);
 					$("#venueIcon").attr("src", venueDetails.icon.iconUrl);
 					$("#venueCategory").text(venueDetails.category.name + " (" + venueDetails.category.parent + ")");
-					$("#venueValue").text(venueDetails.value);
-					$("#venueRent").text(venueDetails.rent);
+					$("#venueValue").text(formatCurrency(venueDetails.value, "€"));
+					$("#venueRent").text(formatCurrency(venueDetails.rent, "€"));
 					// Enable sell button
     				$("#selBtn").show();
 					if(venueDetails.state == 'MINE'){
@@ -48,20 +48,20 @@ $('table tr').click(function(event) {
 
 //Sell button event
 $("#selBtn").click(function() {
-	console.log('SELL CLICKED');
-	console.log('VENUE: ' + selectedVenue);
+	//console.log('SELL CLICKED');
+	//console.log('VENUE: ' + selectedVenue);
 	// Request to SellAction
 	var url = '/SellAction';
 	var jsonData = { "venue":selectedVenue };
-	console.log(jsonData);
-	console.log('REQUEST URL: %j', url);
+	//console.log(jsonData);
+	//console.log('REQUEST URL: %j', url);
     $.ajax(
 		{
             type: 'POST',
             url: '/SellAction',
             data: jsonData,
             success: function (result) {
-				console.log('SellAction REQUEST RESULT: ', JSON.stringify(result));
+				//console.log('SellAction REQUEST RESULT: ', JSON.stringify(result));
 				//Success message
 				$("#scsMsgContent").text(result.message.text);
 				$("#okMsgModal").show();
@@ -82,20 +82,20 @@ $("#selBtn").click(function() {
 
 //Mortgage button event
 $("#mtgBtn").click(function() {
-	console.log('MORTGAGE CLICKED');
-	console.log('VENUE: ' + selectedVenue);
+	//console.log('MORTGAGE CLICKED');
+	//console.log('VENUE: ' + selectedVenue);
 	// Request to MortgageAction
 	var url = '/MortgageAction';
 	var jsonData = { "venue":selectedVenue };
-	console.log(jsonData);
-	console.log('REQUEST URL:' , url);
+	//console.log(jsonData);
+	//console.log('REQUEST URL:' , url);
     $.ajax(
 		{
             type: 'POST',
             url: '/MortgageAction',
             data: jsonData,
             success: function (result) {
-				console.log('MortgageAction REQUEST RESULT: ', JSON.stringify(result));
+				//console.log('MortgageAction REQUEST RESULT: ', JSON.stringify(result));
 				//Success message
 				$("#scsMsgContent").text(result.message.text);
 				$("#okMsgModal").show();
@@ -116,20 +116,20 @@ $("#mtgBtn").click(function() {
 
 //Redeem button event
 $("#rdmBtn").click(function() {
-	console.log('REDEEM CLICKED');
-	console.log('VENUE: ' + selectedVenue);
+	//console.log('REDEEM CLICKED');
+	//console.log('VENUE: ' + selectedVenue);
 	// Request to RedeemAction
 	var url = '/RedeemAction';
 	var jsonData = { "venue":selectedVenue };
-	console.log(jsonData);
-	console.log('REQUEST URL: ', url);
+	//console.log(jsonData);
+	//console.log('REQUEST URL: ', url);
     $.ajax(
 		{
             type: 'POST',
             url: '/RedeemAction',
             data: jsonData,
             success: function (result) {
-				console.log('RedeemAction REQUEST RESULT: ', JSON.stringify(result));
+				//console.log('RedeemAction REQUEST RESULT: ', JSON.stringify(result));
 				//Success message
 				$("#scsMsgContent").text(result.message.text);
 				$("#okMsgModal").show();
@@ -151,11 +151,23 @@ $("#rdmBtn").click(function() {
 // Ok message modal event
 $("#okMsgModal").click(function() {
 	//Reload venues table
-	console.log('OK button clicked!');
+	//console.log('OK button clicked!');
 });
 
 // No message modal event
 $("#noMsgModal").click(function() {
 	//Reload venues table
-	console.log('NO button clicked!');
+	//console.log('NO button clicked!');
 });
+
+function formatCurrency(n, currency) {
+    if(typeof currency != 'undefined'){
+        return currency + " " + n.toFixed().replace(/./g, function(c, i, a) {
+            return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "." + c : c;
+        });
+    }else{
+        return n.toFixed().replace(/./g, function(c, i, a) {
+            return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "." + c : c;
+        });        
+    }
+}
